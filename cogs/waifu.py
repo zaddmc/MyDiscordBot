6a7c4f3418cf16217d501ed4c3e87ec9b69626eb
zaddmc <davidbk927@gmail.com>
2026-05-13 15:00:52 +0200

    Oneshot surely

--- cogs/waifu.py
+++ cogs/waifu.py
@@ -18,6 +18,8 @@ def get_tags() -> dict[str, str]:
         return {"no-tags": "Sorry, failed to fetch tags"}
     tags = {}
     for item in resp.json()["items"]:
+        if item["totalCount"] == 0:
+            continue
         desc = item["name"] + " - " + item["description"]
         if len(desc) > 100:
             desc = desc[:96] + "..."
